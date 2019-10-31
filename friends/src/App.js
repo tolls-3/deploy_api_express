086f0a017d32285a85edcfe111892296c69405ad
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-10-31 12:19:41 +0100

    react app

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get((process.env.REACT_APP_API || "") + "/api/friends")
+      .then(res => {
+        setData(res.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{data && data[0].name}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
