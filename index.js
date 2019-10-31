766d064a2c2071debee1ed93562dbef418c5d54a
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-10-31 10:29:53 +0100

    initial

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT;
+const friends = [
+  { id: 1, name: "Shaun" },
+  { id: 2, name: "Pere" },
+  { id: 3, name: "Megan" }
+];
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/friends", (req, res, next) => {
+  res.json(friends);
+});
+
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
